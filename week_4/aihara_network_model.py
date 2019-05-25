19ee6c983ed28c2b64a8bc4e9b2dd0ae8ca725ad
Sthyao <qq1171098650@163.com>
2019-05-25 21:20:11 +0800

    no message

---
+++ week_4/aihara_network_model.py
@@ -0,0 +1,136 @@
+import numpy as np
+import lab
+import random
+
+a0 = 2
+ar = 10
+kf = 0.15
+kr = 0.9
+t = 200
+
+a = np.array((
+        1,1,0,0,0,0,0,0,1,1,
+        1,1,1,0,0,0,0,1,1,1,
+        0,1,1,1,0,0,1,1,1,0,
+        0,0,1,1,1,1,1,1,0,0,
+        0,0,0,1,1,1,0,0,0,0,
+        0,0,0,0,1,1,1,0,0,0,
+        0,0,1,1,1,1,1,1,0,0,
+        0,1,1,1,0,0,1,1,1,0,
+        1,1,1,0,0,0,0,1,1,1,
+        1,1,0,0,0,0,0,0,1,1
+))
+
+b = np.array((
+    0,0,0,0,0,1,0,0,0,0,
+    0,0,0,0,1,1,1,0,0,0,
+    0,0,0,0,1,1,1,0,0,0,
+    0,0,0,1,1,1,1,1,0,0,
+    0,0,0,1,1,0,1,1,0,0,
+    0,0,1,1,1,0,1,1,1,0,
+    0,0,1,1,0,0,0,1,1,0,
+    0,1,1,1,0,0,0,1,1,1,
+    0,1,1,1,1,1,1,1,1,1,
+    0,1,1,1,1,1,1,1,1,1,
+))
+
+c = np.array((
+    0,0,1,1,1,0,0,0,1,1,
+    0,1,1,1,1,1,1,1,1,1,
+    1,1,1,0,1,1,1,1,0,0,
+    1,1,0,0,0,1,1,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,
+    0,0,0,1,1,0,0,0,1,1,
+    0,0,1,1,1,1,0,1,1,1,
+    1,1,1,1,1,1,1,1,1,0,
+    1,1,0,0,0,1,1,1,0,0,
+    0,0,0,0,0,0,0,0,0,0
+))
+
+d = np.array((
+    0,0,1,0,0,0,0,1,0,0,
+    0,0,1,1,0,0,1,1,0,0,
+    0,0,1,1,1,1,1,1,0,0,
+    0,0,1,1,1,1,1,1,0,0,
+    0,0,1,1,1,1,1,1,0,0,
+    0,1,1,1,1,1,1,1,1,0,
+    1,1,1,1,1,1,1,1,1,1,
+    0,0,0,1,1,1,1,0,0,0,
+    0,0,0,0,1,1,1,0,0,0,
+    0,0,0,0,0,1,0,0,0,0
+))
+
+pettern0 = [a,b,c,d]
+
+x1 = np.zeros(100)
+
+for i in range(100):
+        a = random.randint(0,1)
+        x1[i] = a
+
+
+"""
+def fw(pettern,i,j):
+    wij = 0
+    for k in range(4):
+        wij += (pettern[k][i]-0.5)*(pettern[k][j]-0.5)
+    return wij
+
+def fn(pettern,x,i):
+    w_sum = 0
+    for j in range(100):
+        w_sum += fw(pettern,i,j)*x[j]
+    return w_sum + 0
+
+def fc(x,a):
+    return 0 - ar*x + a
+
+def y(pettern,x,i,a):
+    return fn(pettern,x,i) + fc(x,a)
+"""
+
+x = np.zeros((5001,4,100))
+ni = np.zeros((5001,4,100))
+ci = np.zeros((5001,4,100))
+
+x[0][0] = pettern0[0]
+x[0][1] = pettern0[1]
+x[0][2] = pettern0[2]
+x[0][3] = pettern0[3]
+
+print(lab.hamming(x1, x[0][0]))
+print(lab.hamming(x1, x[0][1]))
+print(lab.hamming(x1, x[0][2]))
+print(lab.hamming(x1, x[0][3]))
+
+
+wij = np.zeros((100,100)) 
+
+for i in range(100):
+        for j in range(100):
+                for p in range(4):
+                        wij[i][j] += (2*x[0][p][i]-1)*(2*x[0][p][j]-1)
+                wij[i][j] = wij[i][j]/4
+process_bar = lab.ShowProcess(t,"ok")
+
+x[0][0] = x1
+
+for t in range(t):
+        for n in range(4):
+                for i in range(100):
+                        wij_some = 0
+                        for j in range(100):
+                                wij_some += wij[i][j]*x[t][n][j]
+                        ni[t+1][n][i] = kf*ni[t][n][i] + wij_some
+                        ci[t+1][n][i] = kr*ci[t][n][i] - ar*x[t][n][i] + a0
+                        x[t+1][n][i] = lab.sigmoid(ni[t+1][n][i]+ci[t+1][n][i])     
+        lab.number_to_image(x[t][0],t)
+        process_bar.show_process()  
+
+
+"""
+lab.number_to_image(x[0][0],10001)
+lab.number_to_image(x[0][1],10002)
+lab.number_to_image(x[0][2],10003)
+lab.number_to_image(x[0][3],10004)
+"""
---
+++ week_4/lab.py
@@ -0,0 +1,103 @@
+import math
+import numpy as np
+import sys
+import time
+from PIL import Image 
+
+k = 0.5
+b = 0.015
+A = 1
+#init
+
+def hamming(array0, array1):
+        sum = 0
+        for i in range(len(array0)):
+                if array0[i] == array1[i]:
+                        sum += 1
+        return sum
+
+def f(x,a):
+    return k * x - A * ( 1 / ( 1 + math.exp( -1 * x / b))) + a
+
+def ly(x):
+    return abs( k + A * math.exp( -1 * x / b) / ( b * ( 1 + math.exp( -1 * x / b ))**2))
+
+def sigmoid(x):
+    if -x/b >= 25:
+        return 1/(1+math.exp(25))
+    else:
+        return 1/(1+math.exp(-x/b))
+
+def number_to_image(a,id):
+        img = Image.new("RGB",(100,100))
+        pixTupleB = (0,0,0,15)
+        pixTupleW = (255,255,255,15)
+        if a[0] >= 0.5:
+                sum_ = np.ones((10,10))
+        else:
+                sum_ = np.zeros((10,10))
+        for j in range(1,10):
+                if a[j] >= 0.5 :
+                        temp = np.ones((10,10))
+                else:
+                        temp = np.zeros((10,10))
+                sum_ = np.hstack((sum_,temp))
+        sum_0 = sum_
+        for i in range(1,10):
+                if a[i*10] >= 0.5:
+                                sum_ = np.ones((10,10))
+                else:
+                        sum_ = np.zeros((10,10))
+                for j in range(1,10):
+                        if a[i*10+j] >= 0.5:
+                                temp = np.ones((10,10))
+                        else:
+                                temp = np.zeros((10,10))
+                        sum_ = np.hstack((sum_,temp))
+                sum_0 = np.vstack((sum_0,sum_))
+        for i in range(100):
+                for j in range(100):
+                        if sum_0[j][i] == 1:
+                                img.putpixel((i,j),pixTupleB)
+                        else:
+                                img.putpixel((i,j),pixTupleW)
+        string = str(id) + '.png'
+        #img.save( 'd:\Git\chaos\week_3\py2\\'+string, 'PNG')
+        img.save('py/'+string,'PNG')
+        #img.show()
+
+def u(x,t):
+        sum = 0
+        for i in range(100):
+                sum += abs(x[t][i] - x[t][i])
+
+class ShowProcess():
+        i = 0 #now
+        max_steps = 0 #all times
+        max_arrow = 20 #length of bar
+        infoDone = 'done'
+
+        def __init__(self,max_steps,infoDone = 'Done'):
+                self.max_steps = max_steps
+                self.i = 0
+                self.infoDone = infoDone
+
+        def show_process(self , i = None):
+                if i is not None:
+                        self.i = i
+                else:
+                        self.i += 1
+                num_arrow = int(self.i * self.max_arrow / self.max_steps)
+                num_line = self.max_arrow - num_arrow
+                percent = self.i * 100.0 / self.max_steps
+                process_bar = '[' + '#'*num_arrow + '-'*num_line + ']' + '%.2f' % percent +'%' + '\r'
+                        
+                sys.stdout.write(process_bar) 
+                sys.stdout.flush()
+                if self.i >= self.max_steps:
+                        self.close()
+        
+        def close(self):
+                print('')
+                print(self.infoDone)
+                self.i = 0
